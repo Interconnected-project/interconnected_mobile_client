fd68502765117a29973eb12b5efa5c8403d4c400
Tale152 <alessandro.talmi@studio.unibo.it>
2022-09-09 21:35:36 +0200

    moved grid status style

--- src/common/styles.ts
+++ src/common/styles.ts
@@ -63,28 +63,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export const gridStatusStyle = {
-  stepIndicatorSize: 25,
-  currentStepIndicatorSize: 25,
-  separatorStrokeWidth: 4,
-  currentStepStrokeWidth: 4,
-  stepStrokeCurrentColor: '#c80a50',
-  stepStrokeWidth: 4,
-  stepStrokeFinishedColor: '#c80a50',
-  stepStrokeUnFinishedColor: '#aaaaaa',
-  separatorFinishedColor: '#fab400',
-  separatorUnFinishedColor: '#aaaaaa',
-  stepIndicatorFinishedColor: '#f0f0f0',
-  stepIndicatorUnFinishedColor: '#f0f0f0',
-  stepIndicatorCurrentColor: '#f0f0f0',
-  stepIndicatorLabelFontSize: 13,
-  currentStepIndicatorLabelFontSize: 13,
-  stepIndicatorLabelCurrentColor: '#f0f0f0',
-  stepIndicatorLabelFinishedColor: '#f0f0f0',
-  stepIndicatorLabelUnFinishedColor: '#f0f0f0',
-  labelColor: '#999999',
-  labelSize: 13,
-  currentStepLabelColor: '#c80a50',
-};
-
 export default styles;
--- src/tabs/home/GridStatus.tsx
+++ src/tabs/home/GridStatus.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import StepIndicator from 'react-native-step-indicator';
-import { gridStatusStyle } from '../../common/styles';
 
+const gridStatusStyle = {
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize: 25,
+  separatorStrokeWidth: 4,
+  currentStepStrokeWidth: 4,
+  stepStrokeCurrentColor: '#c80a50',
+  stepStrokeWidth: 4,
+  stepStrokeFinishedColor: '#c80a50',
+  stepStrokeUnFinishedColor: '#aaaaaa',
+  separatorFinishedColor: '#fab400',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor: '#f0f0f0',
+  stepIndicatorUnFinishedColor: '#f0f0f0',
+  stepIndicatorCurrentColor: '#f0f0f0',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 13,
+  stepIndicatorLabelCurrentColor: '#f0f0f0',
+  stepIndicatorLabelFinishedColor: '#f0f0f0',
+  stepIndicatorLabelUnFinishedColor: '#f0f0f0',
+  labelColor: '#999999',
+  labelSize: 13,
+  currentStepLabelColor: '#c80a50',
+};
 const labels = ['Setup required', 'Connecting', 'Online'];
 
 export default function GridStatus() {
