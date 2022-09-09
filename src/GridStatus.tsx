07388d8e1534458099044e2eb48ad9988903c5db
Tale152 <alessandro.talmi@studio.unibo.it>
2022-09-09 17:27:49 +0200

    moved GridStatus's style to styles.ts

--- src/GridStatus.tsx
+++ src/GridStatus.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import StepIndicator from 'react-native-step-indicator';
+import { gridStatusStyle } from './common/styles';
 
 const labels = ['Setup required', 'Connecting', 'Online'];
-const customStyles = {
-  stepIndicatorSize: 25,
-  currentStepIndicatorSize: 25,
-  separatorStrokeWidth: 4,
-  currentStepStrokeWidth: 4,
-  stepStrokeCurrentColor: '#c80a50',
-  stepStrokeWidth: 4,
-  stepStrokeFinishedColor: '#c80a50',
-  stepStrokeUnFinishedColor: '#aaaaaa',
-  separatorFinishedColor: '#fab400',
-  separatorUnFinishedColor: '#aaaaaa',
-  stepIndicatorFinishedColor: '#f0f0f0',
-  stepIndicatorUnFinishedColor: '#f0f0f0',
-  stepIndicatorCurrentColor: '#f0f0f0',
-  stepIndicatorLabelFontSize: 13,
-  currentStepIndicatorLabelFontSize: 13,
-  stepIndicatorLabelCurrentColor: '#f0f0f0',
-  stepIndicatorLabelFinishedColor: '#f0f0f0',
-  stepIndicatorLabelUnFinishedColor: '#f0f0f0',
-  labelColor: '#999999',
-  labelSize: 13,
-  currentStepLabelColor: '#c80a50',
-};
 
 export default function GridStatus() {
   return (
     <StepIndicator
-      customStyles={customStyles}
+      customStyles={gridStatusStyle}
       currentPosition={2}
       labels={labels}
       stepCount={labels.length}
